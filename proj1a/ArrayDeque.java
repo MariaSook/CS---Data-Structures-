09f105654a515ce09925a2e98335cf821cac67a5
Maria Sooklaris <mariasooklaris@berkeley.edu>
2018-02-03 16:45:54 -0800

    project

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -29,7 +29,7 @@ public class ArrayDeque<T> {
 
     }
 
-    //@Gabby Shvartsman helped me visualize what resize was supposed to do
+    //@Gabby Shvartsman helped me visualize what resize was supposed to do, and a good way to approach this problem
     private void resize(int resizeval) {
         T[] newArray = (T[]) new Object[resizeval];
         int firstvalindex = nextFirst + 1;
@@ -50,12 +50,12 @@ public class ArrayDeque<T> {
 
     private void nflc(){
         /*next first length change*/
-        nextFirst = Math.floorMod(nextFirst - 1, items.length);
+        nextFirst = (nextFirst - 1) % items.length;
     }
 
     private void nllc(){
         /*next last length change*/
-        nextLast = Math.floorMod(nextLast - 1, items.length);
+        nextLast = (nextLast - 1) % items.length;
     }
 
     public void addFirst(T item) {
@@ -69,6 +69,7 @@ public class ArrayDeque<T> {
         nflc();
     }
 
+    //@Andrew Crowley and I worked on adds/removes in the same room, and we talked our way through the problems/debugging we both faced
     public void addLast(T item) {
         /*Adds an item of type T to the back of the array. */
         int sizehold = size();
@@ -82,38 +83,36 @@ public class ArrayDeque<T> {
 
     public T removeFirst() {
         /*Removes and returns the item at the front of the deque.
-        If no such
-        item exists, returns null.*/
+        If no such item exists, returns null.*/
         if (size == 0){
             return null;
-        } else if (items[nextLast-1] == null){
+        } else if (items[(nextFirst-1)%items.length] == null){
             return null;
         } else if (size * 4 < items.length && items.length >= 16) {
             resize(items.length / 2);
         }
         nflc();
-        T holdval = items[nextFirst];
+        T returnval = items[nextFirst];
         items[nextFirst] = null;
         size -= 1;
-        return holdval;
+        return returnval;
     }
 
     public T removeLast() {
         /*Removes and returns the item at the back of the deque.
-        If no such
-        item exists, returns null.*/
+        If no such item exists, returns null.*/
         if (size == 0){
             return null;
-        } else if (items[nextLast-1] == null){
+        } else if (items[(nextLast+1)%items.length] == null){
             return null;
         } else if (size * 4 < items.length && items.length >= 16) {
             resize(items.length / 2);
         }
         nllc();
-        T holdval = items[nextLast];
+        T returnval = items[nextLast];
         items[nextLast] = null;
         size -= 1;
-        return holdval;
+        return returnval;
     }
 
     public boolean isEmpty() {
