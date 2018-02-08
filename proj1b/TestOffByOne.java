d0e70302a36229647780740a1a9eed7b329f7525
Maria Sooklaris <mariasooklaris@berkeley.edu>
2018-02-08 14:51:28 -0800

    final

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -10,10 +10,16 @@ public class TestOffByOne {
     public void testequalChars() {
         assertTrue(offByOne.equalChars('a', 'b'));
         assertTrue(offByOne.equalChars('r', 'q'));
+        assertTrue(offByOne.equalChars(';',':'));
+        assertTrue(offByOne.equalChars('z','{'));
+        assertTrue(offByOne.equalChars(']','^'));
+
 
         assertFalse(offByOne.equalChars('a', 'a'));
         assertFalse(offByOne.equalChars('a', 'e'));
         assertFalse(offByOne.equalChars('z', 'a'));
+        assertFalse(offByOne.equalChars(';', 'p'));
+        assertFalse(offByOne.equalChars('[', '^'));
     }
 
 }
