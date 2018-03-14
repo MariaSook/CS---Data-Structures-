3e7ac4a6996f5b226c5d6a6b2a255726c6a0ad29
Maria Sooklaris <mariasooklaris@berkeley.edu>
2018-03-13 22:20:42 -0700

    gonna kms

--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -5,15 +5,13 @@ import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
     private int T;
-    private int N;
     private double[] tDoubleData;
-    private Percolation perc;
-    private PercolationFactory pf;
     private double percfrac;
     private double mean;
     private double stddev;
     private double conlow;
     private double conhigh;
+    private double sumpercfrac;
 
     // @help from Gabby Schvartsmann
     public PercolationStats(int N, int T, PercolationFactory pf) {
@@ -21,32 +19,35 @@ public class PercolationStats {
         if (N <= 0 || T <= 0) {
             throw new IllegalArgumentException("Illegal Argument");
         }
-        this.N = N;
         this.T = T;
-        this.pf = pf;
         this.tDoubleData = new double[T];
+        this.sumpercfrac = 0;
 
         for (int i = 0; i < T; i++) {
-            this.perc = pf.make(N);
-            while (!perc.percolates()) {
-                int row = StdRandom.uniform(0, N);
-                int col = StdRandom.uniform(0, N);
-                perc.open(row, col);
+            Percolation percolation = pf.make(N);
+            while (!percolation.percolates()) {
+                int row = StdRandom.uniform(N);
+                int col = StdRandom.uniform(N);
+                percolation.open(row, col);
             }
-            percfrac = perc.numberOfOpenSites() / (N * N);
+            percfrac = percolation.numberOfOpenSites() / (N * N);
             tDoubleData[i] = percfrac;
+            sumpercfrac += percfrac;
         }
 
     }
 
     public double mean() {
         // sample mean of percolation threshold
-        mean = StdStats.mean(tDoubleData);
+        mean = sumpercfrac/T;
+        //mean = StdStats.mean(tDoubleData);
         return mean;
     }
 
     public double stddev() {
         // sample standard deviation of percolation threshold
+
+
         stddev = StdStats.stddev(tDoubleData);
         return stddev;
     }
@@ -57,11 +58,12 @@ public class PercolationStats {
         conlow = mean - ((1.96 * stddev) / sqrtT);
         return conlow;
     }
+
     public double confidenceHigh() {
         // high endpoint of 95% confidence interval
         double sqrtT = Math.sqrt(T);
-        conlow = mean + ((1.96 * stddev) / sqrtT);
-        return conlow;
+        conhigh = mean + ((1.96 * stddev) / sqrtT);
+        return conhigh;
     }
 
 }
