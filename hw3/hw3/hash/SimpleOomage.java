7f0723b8f9e3ece7691a2789b4f50217e64041a4
Maria Sooklaris <mariasooklaris@berkeley.edu>
2018-03-17 14:50:25 -0700

    rlly no idea

--- hw3/hw3/hash/SimpleOomage.java
+++ hw3/hw3/hash/SimpleOomage.java
@@ -41,7 +41,7 @@ public class SimpleOomage implements Oomage {
             return red + green + blue;
         } else {
             // replace line
-            return (red * 32 * 32 * 100) + (green * 32 * 10) + blue;
+            return (red * 32 * 32 * 100000) + (green * 32 * 100) + blue;
         }
     }
 
