c78f68aa10dcd835da11fd02411260a1be393263
Maria Sooklaris <mariasooklaris@berkeley.edu>
2018-04-04 03:46:46 -0700

    test round 2 plz

--- hw4/hw4/puzzle/Solver.java
+++ hw4/hw4/puzzle/Solver.java
@@ -1,39 +1,51 @@
 package hw4.puzzle;
 
 import edu.princeton.cs.algs4.MinPQ;
-import java.util.Set;
-import java.util.HashSet;
+import edu.princeton.cs.algs4.Stack;
+
 import java.util.Comparator;
+import java.util.HashSet;
+import java.util.TreeSet;
+import java.util.Set;
 
 public class Solver {
     private MinPQ nodes;
     private int moves;
     private Set<WorldState> seen;
-    private Set<WorldState> returnvals;
+    private Stack<WorldState> returnvals;
+
 
     public Solver(WorldState initial) {
         SearchNode initialSN = new SearchNode(initial, 0, null);
         SNComparator comp = new SNComparator();
         this.nodes = new MinPQ(comp);
         this.seen = new HashSet<>();
-        this.returnvals = new HashSet<>();
+        this.returnvals = new Stack();
 
         nodes.insert(initialSN);
         seen.add(initial);
-
         solverHelper();
+    }
+
+    private void findPrevious(SearchNode curr) {
+        if (curr.previous == null) {
+            returnvals.push(curr.ws);
+            return;
+        }
+        returnvals.push(curr.ws);
+        findPrevious(curr.previous);
     }
 
     private void solverHelper() {
         SearchNode curr = (SearchNode) nodes.delMin();
         WorldState currws = curr.returnws();
-        returnvals.add(currws);
         if (currws.isGoal()) {
+            findPrevious(curr);
             this.moves = curr.moves;
             return;
         } else {
-            for (WorldState n: currws.neighbors()) {
-                if (!seen.contains(n)) {
+            for (WorldState n : currws.neighbors()) {
+                if (!seen.contains(n) && !n.equals(curr.previous)) {
                     SearchNode me = new SearchNode(n, curr.moves + 1, curr);
                     nodes.insert(me);
                 }
@@ -45,7 +57,7 @@ public class Solver {
     }
 
     public int moves() {
-      return this.moves;
+        return this.moves;
     }
 
     public Iterable<WorldState> solution() {
@@ -65,22 +77,21 @@ public class Solver {
         }
 
         private int priotiy() {
-            WorldState me = returnws();
-            return this.moves + me.estimatedDistanceToGoal();
+            return moves + ws.estimatedDistanceToGoal();
         }
 
         private WorldState returnws() {
-            return this.ws;
+            return ws;
         }
     }
 
     private class SNComparator implements Comparator {
         @Override
         public int compare(Object obj1, Object obj2) {
-            Solver.SearchNode me = (Solver.SearchNode) obj1;
-            Solver.SearchNode you = (Solver.SearchNode) obj2;
+            SearchNode me = (SearchNode) obj1;
+            SearchNode you = (SearchNode) obj2;
+
             return me.priotiy() - you.priotiy();
         }
     }
-
 }
