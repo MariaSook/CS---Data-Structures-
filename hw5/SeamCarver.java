b764d2ec74373a9b6b43487cb5e51c0fe0f47bc9
Maria Sooklaris <mariasooklaris@berkeley.edu>
2018-04-25 10:49:18 -0700

    commit

--- hw5/SeamCarver.java
+++ hw5/SeamCarver.java
@@ -2,65 +2,70 @@ import java.awt.Color;
 
 import edu.princeton.cs.algs4.Picture;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.HashMap;
+
 public class SeamCarver {
-    private final Picture picture;
+    private Picture picture;
     private int width;
     private int height;
     private int xpixplus;
     private int xpixminus;
     private int ypixplus;
     private int ypixminus;
+    private HashMap energyMap;
 
     public SeamCarver(Picture picture) {
         this.picture = picture;
+        this.width = picture.width();
+        this.height = picture.height();
+        this.energyMap = new HashMap();
     }
 
     // current picture
     public Picture picture() {
-        Picture newpic = new Picture(picture);
-        return newpic;
+        this.picture = new Picture(picture);
+        return picture;
     }
 
     // width of current picture
     public int width() {
-        this.width = width;
         return picture.width();
     }
 
     // height of current picture
     public int height() {
-        this.height = height;
         return picture.height();
+    }
+
+    private int xvalueCheck(int value) {
+        if (value == 0) {
+            return width - 1;
+        }
+        if (value == width - 1) {
+            return 0;
+        }
+        return value;
+    }
+
+    private int yValueCheck(int value) {
+        if (value == 0) {
+            return height - 1;
+        }
+        if (value == height - 1) {
+            return 0;
+        }
+        return value;
     }
 
     // energy of pixel at column x and row y
     public double energy(int x, int y) {
-        width();
-        height();
-
-        this.xpixminus = x - 1;
-        this.xpixplus = x + 1;
-        this.ypixminus = y - 1;
-        this.ypixplus = y + 1;
-
-        if (x == 0) {
-            xpixminus = width - 1;
-        }
-        if (x == width - 1) {
-            xpixplus = 0;
-        }
-        if (y == 0) {
-            ypixminus = height - 1;
-        }
-        if (y == height - 1) {
-            ypixplus = 0;
-        }
+        this.xpixminus = xvalueCheck(x - 1);
+        this.xpixplus = xvalueCheck(x + 1);
 
         Color x1color = picture.get(xpixminus, y);
         Color x2color = picture.get(xpixplus, y);
-
-        Color y1color = picture.get(x, ypixminus);
-        Color y2color = picture.get(x, ypixplus);
 
         int x1blue = x1color.getBlue();
         int x1red = x1color.getRed();
@@ -70,13 +75,19 @@ public class SeamCarver {
         int x2red = x2color.getRed();
         int x2green = x2color.getGreen();
 
-        int xbluebreak = Math.abs(x2blue - x1blue);
-        int xredbreak = Math.abs(x2red - x1red);
-        int xgreenbreak = Math.abs(x2green - x1green);
+        int xbluebreak = x2blue - x1blue;
+        int xredbreak = x2red - x1red;
+        int xgreenbreak = x2green - x1green;
 
-        double xdiff = Math.pow(xbluebreak, 2)
-                + Math.pow(xredbreak, 2)
-                + Math.pow(xgreenbreak, 2);
+        double xdiff = (xbluebreak * xbluebreak)
+                + (xredbreak * xredbreak)
+                + (xgreenbreak * xgreenbreak);
+
+        this.ypixminus = yValueCheck(y - 1);
+        this.ypixplus = yValueCheck(y + 1);
+
+        Color y1color = picture.get(x, ypixminus);
+        Color y2color = picture.get(x, ypixplus);
 
         int y1blue = y1color.getBlue();
         int y1red = y1color.getRed();
@@ -86,27 +97,80 @@ public class SeamCarver {
         int y2red = y2color.getRed();
         int y2green = y2color.getGreen();
 
-        int ybluebreak = Math.abs(y2blue - y1blue);
-        int xyredbreak = Math.abs(y2red - y1red);
-        int xygreenbreak = Math.abs(x2green - x1green);
+        int ybluebreak = y2blue - y1blue;
+        int yredbreak = y2red - y1red;
+        int ygreenbreak = y2green - y1green;
 
-        double ydiff = Math.pow(ybluebreak, 2)
-                + Math.pow(xyredbreak, 2)
-                + Math.pow(xgreenbreak, 2);
+        double ydiff = (ybluebreak * ybluebreak)
+                + (yredbreak * yredbreak)
+                + (ygreenbreak * ygreenbreak);
 
+        energyMap.put("" + x + "," + y + "", (xdiff + ydiff));
         return xdiff + ydiff;
     }
 
     // sequence of indices for horizontal seam
     public int[] findHorizontalSeam() {
-        int[] ints = new int[4];
-        return ints;
+        ArrayList returnval = new ArrayList();
+        HashSet seen = new HashSet();
+
+        int xValStart = 0;
+        for (int x = 0; x < width; x ++) {
+
+        }
+        int[] temp = new int[1];
+
+        return temp;
     }
 
     // sequence of indices for vertical seam
     public int[] findVerticalSeam() {
-        int[] ints = new int[4];
-        return ints;
+        ArrayList returnVal = new ArrayList();
+
+        int yValStart = Integer.MAX_VALUE;
+        double minEnergy = Double.MAX_VALUE;
+        for (int y = 0; y < width; y ++) {
+            double energyVal = energy(height - 1, y);
+            if (energyVal < minEnergy) {
+                yValStart = y;
+                minEnergy = energyVal;
+            }
+        }
+        returnVal.add(yValStart);
+
+        int startValY = yValStart;
+        int startValX = 0;
+        int tempheight = height - 2;
+        while (height != -1) {
+            height -= 1;
+
+        }
+        int[] temp = new int[1];
+
+        return temp;
+
+    }
+    public int helperEdgeCases(int xValStart, int yValStart) {
+        if (xValStart == 0) {
+
+        }
+        else if (xValStart == width - 1) {
+
+        }
+
+        if (yValStart == 0) {
+
+        }
+        else if (yValStart == width - 1) {
+
+        }
+        return 0;
+    }
+
+
+    public int helperVerticalSeam(int xValStart, int yValStart) {
+        return 0;
+
     }
 
     // remove horizontal seam from picture
