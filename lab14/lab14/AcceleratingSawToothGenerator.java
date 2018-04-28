e0335c62ab83675e29442b9fba2e297961a1d252
Maria Sooklaris <mariasooklaris@berkeley.edu>
2018-04-27 18:01:21 -0700

    jksdlfjs

--- lab14/lab14/AcceleratingSawToothGenerator.java
+++ lab14/lab14/AcceleratingSawToothGenerator.java
@@ -4,6 +4,10 @@ import lab14lib.Generator;
 
 public class AcceleratingSawToothGenerator implements Generator {
 
+    public AcceleratingSawToothGenerator(int x, double y) {
+
+    }
+
     public double next() {
         return 0;
     }
--- lab14/lab14/StrangeBitwiseGenerator.java
+++ lab14/lab14/StrangeBitwiseGenerator.java
@@ -4,6 +4,11 @@ import lab14lib.Generator;
 
 public class StrangeBitwiseGenerator implements Generator{
 
+    public StrangeBitwiseGenerator(int x) {
+
+    }
+
+
     public double next() {
         return 0;
     }
