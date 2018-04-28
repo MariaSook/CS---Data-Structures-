5ed3fe48f30c9fd66f6e139ecfdefda1386fa97b
Maria Sooklaris <mariasooklaris@berkeley.edu>
2018-04-27 18:04:34 -0700

    jksdlfjs

--- lab14/lab14/AcceleratingSawToothGenerator.java
+++ lab14/lab14/AcceleratingSawToothGenerator.java
@@ -3,12 +3,35 @@ package lab14;
 import lab14lib.Generator;
 
 public class AcceleratingSawToothGenerator implements Generator {
+    private int period;
+    private int state;
+    private double factor;
 
-    public AcceleratingSawToothGenerator(int x, double y) {
-
+    public AcceleratingSawToothGenerator(int period, double factor) {
+        this.period = period;
+        this.factor = factor;
+        this.state = 0;
     }
 
     public double next() {
-        return 0;
+        if (state == period) {
+            state = 0;
+            return normalize(state);
+        }
+        state += 1;
+        return normalize(state);
+    }
+
+    private double normalize(double normalized) {
+        normalized = 2 * (normalized / period) - 1;
+        if (normalized == -1) {
+            period = (int) (period * factor);
+            period = Math.max(period, 1);
+        }
+        if (state == 0) {
+            period = (int) (period * factor);
+            period = Math.max(period, 1);
+        }
+        return normalized;
     }
 }
