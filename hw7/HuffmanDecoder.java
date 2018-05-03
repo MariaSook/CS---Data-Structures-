444fac3c5beb67a7dfebde95733204cb69de794a
Maria Sooklaris <mariasooklaris@berkeley.edu>
2018-05-02 23:54:28 -0700

    last submit

--- hw7/HuffmanDecoder.java
+++ hw7/HuffmanDecoder.java
@@ -1,19 +1,19 @@
 public class HuffmanDecoder {
 
     public static void main(String[] args) {
-        ObjectReader objectreader = new ObjectReader(args[0]);
-        Object one = objectreader.readObject();
-        Object two = objectreader.readObject();
-        Object three = objectreader.readObject();
-        BinaryTrie hTrie = (BinaryTrie) one;
+        ObjectReader objectReader = new ObjectReader(args[0]);
+        Object one = objectReader.readObject();
+        Object two = objectReader.readObject();
+        Object three = objectReader.readObject();
+        BinaryTrie htrie = (BinaryTrie) one;
         int numsymbols = (int) two;
-        BitSequence bits = (BitSequence) three;
-        char[] originTxt = new char[numsymbols];
+        BitSequence b = (BitSequence) three;
+        char[] text = new char[numsymbols];
         for (int i = 0; i < numsymbols; i++) {
-            Match match = hTrie.longestPrefixMatch(bits);
-            originTxt[i] = match.getSymbol();
-            bits = bits.allButFirstNBits(match.getSequence().length());
+            Match match = htrie.longestPrefixMatch(b);
+            text[i] = match.getSymbol();
+            b = b.allButFirstNBits(match.getSequence().length());
         }
-        FileUtils.writeCharArray(args[1], originTxt);
+        FileUtils.writeCharArray(args[1], text);
     }
 }
